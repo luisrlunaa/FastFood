bb597ae2446561e4071d86b56f623af7bdd93e6e
Luiqui Cash <luisrlunaa@outlook.com>
2022-11-20 10:30:58 -0400

    Add default icon

--- FastFood/Properties/Resources.Designer.cs
+++ FastFood/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FastFoodDemo.Properties {
+namespace FastFood.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace FastFoodDemo.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FastFoodDemo.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FastFood.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
